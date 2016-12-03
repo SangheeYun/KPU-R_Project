49766f4fb6c68019dac04ec73deea9ac83646f07
Yuni_24 <yunsh24@gmail.com>
2016-12-03 19:21:43 +0900

    Add WaitTime Data, Analysis about CallWaitTime.

--- R_Project.R
+++ R_Project.R
@@ -23,7 +23,7 @@ Data <- read.csv("~/Documents/KPUDocument/Programming/DataFile/PeopleFac.csv")
 Subway <- read.csv("~/Documents/KPUDocument/Programming/DataFile/amongSubway.csv")
 SubwayFree <- read.csv("~/Documents/KPUDocument/Programming/DataFile/SubwayFree.csv")
 CallTime <- read.csv("~/Documents/KPUDocument/Programming/DataFile/dailyCallTime.csv", header = FALSE) #월별 시간대별 콜택시 이용현황
-
+WaitTime <- read.csv("~/Documents/KPUDocument/Programming/DataFile/dailyWait.csv")
 
 #################################장애인 생활권 분석#####################################
 
@@ -52,6 +52,28 @@ dataOfArea[order(dataOfArea$Destinetion_Times, decreasing = FALSE), ]
 
 ####################################시계열 분 석(시간별)#########################################
 
+# 콜택시 대기시간 분석
+m_WaitTime <- as.matrix(WaitTime)
+v_WaitTime <- as.vector(m_WaitTime)
+v_WaitTime.ts <- ts(data = v_WaitTime, start = 1, frequency = 24)
+plot(v_WaitTime.ts)
+abline(a=400, b=0, col="red")
+
+fill_colors = c() # list or vector in color info 
+for (i in 1:length(s1_WaitTime)){ # 40분 이상의 대기가 이루어지는 시간대와 그렇지않은 시간대의 구분
+  if (s1_WaitTime[i] > 40){ # s1 or s2
+    fill_colors = c(fill_colors, "#821122")
+  }else {
+    fill_colors = c(fill_colors, "#cccccc")
+  }
+}
+
+s1_WaitTime <- apply(WaitTime[,1:6], 1, mean)
+barplot(s1_WaitTime, main = "Jan to Jun", col= fill_colors, border = NA)
+s2_WaitTime <- apply(WaitTime[,7:10], 1, mean)
+barplot(s2_WaitTime, main = "Jul to Oct", col= fill_colors, border = NA)
+
+# 콜택시 사용건수 분석
 m_CallTime <- as.matrix(CallTime)
 v_CallTime <- as.vector(m_CallTime)
 v_CallTime.ts <- ts(data = v_CallTime, start = 1, frequency = 24) # 장애인 콜택시 월별 이용현황 시각화 
@@ -59,8 +81,7 @@ plot(v_CallTime.ts)
 abline(a=400, b=0, col="red")
 
 fill_colors = c() # list or vector in color info 
-
-for (i in 1:length(s2_CallTime)){ # 1700명 이상의 교통이동이 이루어지는 시간대와 그렇지않은 시간대의 구분
+for (i in 1:length(s2_CallTime)){ # 400명 이상의 교통이동이 이루어지는 시간대와 그렇지않은 시간대의 구분
   if (s1_CallTime[i] > 400){ # s1 or s2
     fill_colors = c(fill_colors, "#821122")
   }else {
@@ -87,7 +108,9 @@ BusData <- aggregate(LFBusData[,3:52], by = list(LFBusData$BusNumber), FUN=mean)
 BusData <- BusData[c(-1,-2,-3)]
 m_Bus <- as.matrix(BusData)
 m_Bus.ts <- ts(data = m_Bus, start =1, frequency = 48 )
+head(m_Bus.ts)
 barplot(m_Bus.ts, main = "LowFloorBus")
+abline(a=60000, b=0, col="red")
 
 ####################################회 귀 분 석(지역별)#########################################
 cor(dataOfArea$Disabled_Facilities, dataOfArea$Destinetion_Times) # 장애인 시설 - 0.5892295 정상관
@@ -101,4 +124,3 @@ with(dataOfArea, plot(Destinetion_Times, Disabled_Facilities, cex=.7, pch=as.num
 
 plot(Disabled1)
 plot(Disabled2)
-
